061660fa60327cb633ffc300ecdbab9ccb2c88b9
saurabhjn76 <saurabhjn76@gmail.com>
2017-10-21 02:46:48 +0530

    PEP8 formatting

--- database_setup.py
+++ database_setup.py
@@ -1,10 +1,12 @@
-import os, sys
+import os
+import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -13,6 +15,7 @@ class User(Base):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
+
 
 class Sports(Base):
     __tablename__ = 'sports'
@@ -54,8 +57,6 @@ class SportsPlayer(Base):
             'country': self.country,
             'rank': self.rank,
         }
-
-
 
 
 engine = create_engine('sqlite:///sports.db')
